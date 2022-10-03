55299d198f1d15605152ce865cd2435be59703f2
Grace Xin <qingqinggrace@gmail.com>
2022-10-03 00:04:32 -0400

    Fix evaluations page; allow reviewers to add grades

--- docs-core/target/classes/db/update/dbupdate-028-0.sql
+++
@@ -1,2 +0,0 @@
-create memory table T_REVIEWERS (SCO_REVIEWERID_C varchar(36) not null, SCO_SKILLS_N tinyint(5) not null, SCO_EXPERIENCE_N tinyint(5) not null, SCO_HIRE_B boolean default 0)
-update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
