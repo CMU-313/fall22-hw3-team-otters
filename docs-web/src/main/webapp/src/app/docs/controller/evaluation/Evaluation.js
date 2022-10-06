05b70ce2fe8a1303d0dcdf0cc18a70f3a7b08359
Grace Xin <qingqinggrace@gmail.com>
2022-10-05 23:21:13 -0400

    Connect frontend to middleware and backend

--- docs-web/src/main/webapp/src/app/docs/controller/evaluation/Evaluation.js
+++ docs-web/src/main/webapp/src/app/docs/controller/evaluation/Evaluation.js
@@ -5,24 +5,31 @@
  */
 angular.module('docs').controller('Evaluation', function(Restangular, $scope, $state) {
   // Load evaluation
-  Restangular.one('reviewer').get({
-    sort_column: 1,
-    asc: true
-  }).then(function(data) {
-    $scope.evaluation = data.reviewer;
+  
+  Restangular.one('reviewer', 'list').get().then(function(data) {
+    $scope.reviewers = data.reviewers;
   });
 
   // Open a evaluation
-  $scope.openEvaluation = function(reviewer) {
+  $scope.openEvaluation = function (reviewer) {
     $state.go('reviewer', { name: reviewer.name });
   };
 
-  $scope.addEvaluation = function() {
-    $scope.evaluation.push({
-      'Reviewer Name': $scope.reviewer,
-      'Skill': $scope.skill,
-      'Experience': $scope.experience,
-      'Hire': $scope.hire
+  $scope.addEvaluation = function () {
+    Restangular.one('reviewer').put({
+      name: $scope.reviewer,
+      skill_score: $scope.skill,
+      experience_score: $scope.experience,
+      hire: ($scope.hire == 'no') ? -1 : 1
     });
   };
+
+  $scope.getAverages = function () {
+    Restangular.one('reviewer', 'average').get().then(function(data) {
+      $scope.name_average = data.name;
+      $scope.skill_average = data.skill_score;
+      $scope.experience_average = data.experience_score;
+      $scope.hire_average = data.hire;
+    });
+  }
 });
