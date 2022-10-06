ffe234fe9ea7bccf8a94b5060c033c6fd4c05d94
Grace Xin <qingqinggrace@gmail.com>
2022-10-06 16:32:33 -0400

    Display evaluations and averages in table

--- docs-web/src/main/webapp/src/app/docs/controller/evaluation/Evaluation.js
+++ docs-web/src/main/webapp/src/app/docs/controller/evaluation/Evaluation.js
@@ -3,7 +3,7 @@
 /**
  * Evaluation controller.
  */
-angular.module('docs').controller('Evaluation', function(Restangular, $scope, $state) {
+angular.module('docs').controller('Evaluation', function(Restangular, $window, $scope, $state) {
   // Load evaluation
   
   Restangular.one('reviewer', 'list').get().then(function(data) {
@@ -22,14 +22,21 @@ angular.module('docs').controller('Evaluation', function(Restangular, $scope, $s
       experience_score: $scope.experience,
       hire: ($scope.hire == 'no') ? -1 : 1
     });
+    $window.location.reload();
   };
 
   $scope.getAverages = function () {
     Restangular.one('reviewer', 'average').get().then(function(data) {
-      $scope.name_average = data.name;
-      $scope.skill_average = data.skill_score;
-      $scope.experience_average = data.experience_score;
-      $scope.hire_average = data.hire;
+      var table = document.getElementById("evaluations");
+      var row = table.insertRow();
+      var name_cell = row.insertCell(0);
+      var skill_cell = row.insertCell(1);
+      var experience_cell = row.insertCell(2);
+      var hire_cell = row.insertCell(3);
+      name_cell.innerHTML = data.name;
+      skill_cell.innerHTML = data.skill_score;
+      experience_cell.innerHTML = data.experience_score;
+      hire_cell.innerHTML = data.hire;
     });
   }
 });
